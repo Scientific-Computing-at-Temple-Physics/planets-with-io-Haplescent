ecb05b537dc796570f827751bfb74f22a72e9394
Haplescent <johnmerritt95@gmail.com>
2018-02-01 14:53:58 -0500

    update 1

--- weight_on_planets.py
+++ weight_on_planets.py
@@ -4,7 +4,8 @@ import math as ma
 from sys import argv
 
 """
-filename must follow exact format example
+filename must follow example format
+each item must be separated with a ;
 Mercury;  2439.7 km;  5.427 g/cm^3;
 planet can be lower or uppercase
 altitude must be in meters
@@ -18,13 +19,18 @@ radius_table = {}
 density_table = {}
 numlist = []
 
-for each in infile:
-	line = each.split(";  ")
-	listfile.append(line)
-infile.close()
+def file_list(file, list):
+	#This function will assign items between a ; in an opened file
+	# and assign them to a list
+	for each in file:
+		line = each.split("; ")
+		list.append(line)
+	file.close()
 
 def traverse(o, tree_types=(list, tuple)):
-	#allows to iterate through list of lists
+	#This traverse generator function can be used to iterate over all the values
+	#from Jeremy Banks on stack_overflow
+	#https://stackoverflow.com/questions/6340351/python-iterating-through-list-of-list
     if isinstance(o, tree_types):
         for value in o:
             for subvalue in traverse(value, tree_types):
@@ -34,6 +40,8 @@ def traverse(o, tree_types=(list, tuple)):
 
 def number_extraction(data, list):
 	#extracts floating values and adds them to a list
+	#from jmnas on stack_overflow
+	#https://stackoverflow.com/questions/4289331/python-extract-numbers-from-a-string
 	for each in traverse(data):
 		for t in each.split():
 			try:
@@ -52,6 +60,7 @@ def build_table(data1, data2):
 	else:
 		return "Built radius table"
 
+file_list(infile, listfile)
 number_extraction(listfile, numlist)
 build_table(listfile,numlist)
 
@@ -62,14 +71,14 @@ alt = float(altitude) # altitude will be in meters
 r = radius_table[p] * 1000 # radius will be in meters
 d = density_table[p] * (100 ** 3) / (1000) # density will be in 
 # kg / m^3
-planet_volume = 4 / 3 * (r ** 3) * ma.pi # volume is in m^3
+planet_volume = 4.00 / 3.00 * (r ** 3) * ma.pi # volume is in m^3
 planet_mass = planet_volume * d  # kg
-explorer_force = (6.674e-11) * planet_mass * m / ((alt + r) ** 2) # Newtons
+explorer_force = (6.674 * 10 ** (-11)) * planet_mass * m / ((alt + r) ** 2) # Newtons
 grav_accel = explorer_force / m 
 
-print planet_mass
 print explorer_force
 print grav_accel
+
 
 
 
